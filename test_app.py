22bf736a0e06b10c7d82ee9b4c176849e5443b00
Craig <craigtipple81@gmail.com>
2024-06-04 17:36:59 +0100

    Removed test_utils and added to main test_app file

--- test_app.py
+++ test_app.py
@@ -138,3 +138,24 @@ def test_fetch_cleveland_artworks(client, mocker):
     assert len(data['artworks']) == 2
     assert data['artworks'][0]['title'] == "Artwork 1"
     assert data['artworks'][1]['title'] == "Artwork 2"
+
+# ---- UTILS ---- #
+
+# import pytest
+# import requests_mock
+# from utils.fetch_all_met_object_IDs import fetch_all_met_object_IDs
+
+# @pytest.fixture
+# def mocked_response():
+#     with requests_mock.Mocker() as mocker:
+#         mock_response = {
+#             "total": 1000,
+#             "objectIDs": list(range(1, 1001))
+#         }
+#         mocker.get('https://collectionapi.metmuseum.org/public/collection/v1/objects', json=mock_response)
+#         yield mocker
+
+# def test_fetch_all_met_object_IDs(mocked_response):
+#     result = fetch_all_met_object_IDs()
+    
+#     assert len(result['objectIDs']) == 1000
